51df5c273b0d4e590039662508a5052d71750010
Valeriy Aleksashkin <abc218@ya.ru>
2020-02-24 21:18:22 +0300

    Тестовое задание № 1 cleverence.ru

---
+++ StaticClassServer/Server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace StaticClassServer
+{
+    /// <summary>
+    /// Класс "Сервер" из условия задачи
+    /// </summary>
+    public static class Server
+    {
+        /// <summary>
+        /// Счетчик из условия задачи
+        /// </summary>
+        private static int count = 0;
+        
+        /// <summary>
+        /// Объект для синхронизации потоков
+        /// </summary>
+        private static readonly ReaderWriterLockSlim syncObj = new ReaderWriterLockSlim();
+
+        /// <summary>
+        /// Метод "Сервера" для чтения из count
+        /// </summary>
+        /// <returns>Значение count</returns>
+        public static int GetCount()
+        {
+            // Хранитель результата считывания
+            int result;
+            // Получим блокировку чтения
+            syncObj.EnterReadLock();
+            try
+            {
+                // Считаем count
+                result = count;
+            }
+            finally
+            {
+                // Отпустим блокировку чтения
+                syncObj.ExitReadLock();
+            }
+            // Вернем считанное значение
+            return result;
+        }
+
+        /// <summary>
+        /// Метод "Сервера" для записи в count
+        /// </summary>
+        /// <param name="value">Число, которое нужно прибавить к count</param>
+        public static void AddToCount(int value)
+        {
+            // Получим блокировку записи
+            syncObj.EnterWriteLock();
+            try
+            {
+                // Прибавим значение
+                count += value;
+            }
+            finally
+            {
+                // Отпустим блокировку записи
+                syncObj.ExitWriteLock();
+            }
+        }
+
+    }
+}
