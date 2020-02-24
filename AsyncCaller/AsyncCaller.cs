5da9252627c4507759e1b327c150f0d16d604eb4
Valeriy Aleksashkin <abc218@ya.ru>
2020-02-24 22:30:04 +0300

    Тестовое задание № 2 cleverence.ru

---
+++ AsyncCaller/AsyncCaller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AsyncCaller
+{
+    public class AsyncCaller
+    {
+        /// <summary>
+        /// Хранитель переданного делегата
+        /// </summary>
+        private readonly EventHandler h;
+
+        /// <summary>
+        /// Хранитель хендла, выданного BeginInvoke
+        /// </summary>
+        public IAsyncResult asyncResult { get; private set;  }
+
+        /// <summary>
+        /// Конструктор по умолчанию выключен
+        /// </summary>
+        private AsyncCaller() { }
+
+        /// <summary>
+        /// Параметризованный конструктор
+        /// </summary>
+        /// <param name="h">Делегат, который нужно вызвать "полусинхронно"</param>
+        AsyncCaller(EventHandler h)
+        {
+            // Запомним ссылку на переданный делегат
+            this.h = h;
+        }
+
+        /// <summary>
+        /// Вызов переданного конструктору делегата с ожиданием его завершения waitForMillis мс. Если выполнение делегата займет больше waitForMillis мс, то метод вернет управление.
+        /// Независимо от выбранного варианта обработки завершения вызова делегата необходимо всегда использовать метод EndInvoke.
+        /// </summary>
+        /// <param name="waitForMillis">Время ожидания в миллисекундах</param>
+        /// <param name="sender">Первый параметр для BeginInvoke</param>
+        /// <param name="e">Второй параметр для BeginInvoke</param>
+        /// <param name="callback">Третий параметр для BeginInvoke</param>
+        /// <param name="object">Четвертый параметр для BeginInvoke</param>
+        /// <returns>Признак завершения асинхронного вызова</returns>
+#nullable enable
+        public bool Invoke(int waitForMillis, object? sender, EventArgs e, AsyncCallback callback, object @object)
+#nullable disable
+        {
+            // Запустим асинхронное выполнение
+            asyncResult = h.BeginInvoke(sender, e, callback, @object);
+            // Синхронно подождем 
+            asyncResult.AsyncWaitHandle.WaitOne(waitForMillis);
+            // Вернем признак завершения
+            return asyncResult.IsCompleted;
+        }
+
+        /// <summary>
+        /// Реализует EndInvoke для удобства вызова клиентом
+        /// </summary>
+        public void EndInvoke()
+        {
+            h.EndInvoke(asyncResult);
+            asyncResult.AsyncWaitHandle.Close();
+        }
+    }
+}
---
+++ AsyncCaller/readme.md
@@ -0,0 +1,19 @@
+﻿Тестовое задание
+
+В .net есть возможность звать делегаты как синхронно: 
+```
+EventHandler h = new EventHandler(this.myEventHandler); 
+h.Invoke(null, EventArgs.Empty); 
+```
+так и асинхронно:
+```
+var res = h.BeginInvoke(null, EventArgs.Empty, null, null);
+```
+
+Нужно реализовать возможность полусинхронного вызова делегата (написать реализацию класса AsyncCaller), который бы работал таким образом: 
+```
+EventHandler h = new EventHandler(this.myEventHandler); 
+ac = new AsyncCaller(h); 
+bool completedOK = ac.Invoke(5000, null, EventArgs.Empty);
+```
+"Полусинхронного" в данном случае означает, что делегат будет вызван, и вызывающий поток будет ждать, пока вызов не выполнится.  Но если выполнение делегата займет больше 5000 миллисекунд, то ac.Invoke выйдет и вернет в completedOK значение false.
